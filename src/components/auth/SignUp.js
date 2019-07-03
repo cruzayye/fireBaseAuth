4e4a204eec3df86c5b205150066b14957ca19944
cruzayye <johnnyc010392@gmail.com>
2019-07-03 14:01:48 -0700

    user showing up in firebase but getting a permission denied warning in console

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import styles from './SignUp.css';
+import { register } from '../services/firebaseConfig';
 
 export class SignUp extends Component {
   static propTypes = {
@@ -7,22 +8,54 @@ export class SignUp extends Component {
   }
 
   state = {
-    name: null
-    
+    name: null,
+    lastName: null,
+    email: null,
+    password: null,
   }
+
   handleChange = ({ target }) => {
     this.setState({ [target.name]: target.value });
+  }
+
+  handleSignUp = event => {
+    event.preventDefault();
+    const { name, lastName, email, password } = this.state;
+    const user = { email, password, name, lastName };
+    register(user);
   }
 
   render() {
     return (
       <section className={styles.SignUp}>
         <h2>Sign UP</h2>
-        <form>
-          <input type="name" name="name" onChange={this.handleChange} />
+        <form onSubmit={this.handleSignUp}>
+          <label htmlFor="firstName">First Name</label>
+          <input 
+            id="firstName" 
+            type="name" 
+            name="name" 
+            onChange={this.handleChange} />
+          <label htmlFor="lastName">Last Name</label>
+          <input 
+            id="lastName" 
+            type="name" 
+            name="lastName" 
+            onChange={this.handleChange} />
+          <label htmlFor="email">Email</label>
+          <input 
+            id="email" 
+            type="name" 
+            name="email" 
+            onChange={this.handleChange} />
+          <label htmlFor="email">Password</label>
+          <input 
+            id="passWord" 
+            type="name" 
+            name="password" 
+            onChange={this.handleChange} />
+          <button>submit</button>
         </form>
-
-
       </section>
     );
   }
--- src/components/services/firebaseConfig.js
+++ src/components/services/firebaseConfig.js
@@ -1,4 +1,8 @@
-import * as firebase from 'firebase/app';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+import 'firebase/database';
 
 var firebaseConfig = {
   apiKey: 'AIzaSyCRLYKsvQ5iVS0nNsYpadFKbBP0H_YXaHA',
@@ -10,5 +14,34 @@ var firebaseConfig = {
   appID: 'app-id',
 };
 
+export const app = firebase.initializeApp(firebaseConfig);
+
+const realTimeDb = firebase.database();
+const fireStoreDb = firebase.firestore();
+const fireBaseAuth = firebase.auth;
+const auth = firebase.auth();
+const storage = firebase.storage();
+
+
+//RTDB list of refs
+const userRef = realTimeDb.ref('users');
+
+const singleUserRef = userId => userRef.child(userId);
+
+export const register = (userInfo) => {
+  const { email, password } = userInfo;
+  let user = { ...userInfo };
+  return auth
+    .createUserWithEmailAndPassword(email, password)
+    .then(data => {
+      const { uid } = data.user;
+      singleUserRef(uid).set(user);
+    })
+    .catch(err => {
+      err.type = 'ERROR';
+      console.error(err);
+    });
+};
+
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
