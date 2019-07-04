eaf35f2abc48fde254a9ff2447ebbae4400f1b0c
cruzayye <johnnyc010392@gmail.com>
2019-07-04 13:07:01 -0700

    sign up page styled up

---
+++ src/components/login/Login.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class Login extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return (
+      <h2>
+        login
+      </h2>
+    )
+  }
+}
+
+export default Login
