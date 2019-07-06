0312812ab1fa2da1b3dbb2f872cf3fdb23adcb13
cruzayye <johnnyc010392@gmail.com>
2019-07-06 12:39:26 -0700

    redirect from successful login works

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { login } from '../services/firebaseConfig';
+import { Link, Redirect } from 'react-router-dom';
 import styles from './Login.css';
 
 
@@ -8,29 +9,35 @@ export class Login extends Component {
   state = {
     email: null,
     password: null,
+    wasLoginSuccessful: false,
   }
 
   handleChange = ({ target }) => {
-    this.setState({ [target.name]: target.value})
+    this.setState({ [target.name]: target.value });
   }
 
   handleLogin = async event => {
     event.preventDefault();
-    const {email, password} = this.state;
-    login(email, password)
+    const { email, password } = this.state;
+    login(email, password);
     try {
       const authResult = await login(email, password);
-      console.log(authResult)
-    } catch (error) {
-      console.log('no user', eror)
+      if(authResult.user.uid) {
+        this.setState({ wasLoginSuccessful: true });
+      }
+    } catch(error) {
+      console.log('no user', error);
     }
-
   }
 
   render() {
+    if(this.state.wasLoginSuccessful){
+      return <Redirect to="/signup"/>;
+
+    }
     return (
       <section className={styles.Login}>
-       <h2>Login</h2>
+        <h2>Login</h2>
         <form onSubmit={this.handleLogin}>
           <label htmlFor="email">Email</label>
           <input 
@@ -44,11 +51,12 @@ export class Login extends Component {
             type="name" 
             name="password" 
             onChange={this.handleChange} />
-          <button>submit</button>
+          <button type="submit">login</button>
+          <button><Link to="/signup">signup</Link></button>
         </form>
       </section>
-    )
+    );
   }
 }
 
-export default Login
+export default Login;
